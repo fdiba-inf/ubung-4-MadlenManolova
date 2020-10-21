cf86b0c95d08a41e2e41a5ed7056643c1f8bb2a4
MadlenManolova <madllymanolova@gmail.com>
2020-10-21 15:03:57 +0000

    ArrayOutput

--- src/exercise4/ArrayOutput.java
+++ src/exercise4/ArrayOutput.java
@@ -3,20 +3,23 @@ package exercise4;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class ArrayInputOutput {
+public class ArrayOutput {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int a = input.nextInt ();
         int[] numbers = new int[a];
-
+        
         System.out.println("Enter numbers: ");
         for (int index = 0; index < numbers.length; index++) {
-            numbers[index] = input.nextInt();
+        numbers[index] = input.nextInt();
         }
 
-        String numbersAsString = Arrays.toString(numbers);
-        System.out.println(numbersAsString);
+        for (int index = 0; index < numbers.length; index++) {
+          for (int j = 0; j < index; j++){
+          System.out.print(' '); 
+          }
+          System.out.println(numbers[index]);
+         }
     }
-
 }
